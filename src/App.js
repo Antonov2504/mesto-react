b0ca0f7565959030442f1346112b2f6540b745b1
Denis Antonov <antonov25041992@yandex.ru>
2021-02-03 18:49:14 +0300

    feat: add page-content jsx and styles css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,136 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './images/header__logo.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="page__container">
+      <header className="header">
+        <img src={logo} alt="Место в России" className="header__logo" />
       </header>
+      <main className="content">
+        <section className="profile">
+          <div className="profile__about">
+            <div className="profile__avatar">
+              <img src="#" alt="Имя пользователя" className="profile__avatar-img" />
+            </div>
+            <div className="profile__description">
+              <h1 className="profile__name"></h1>
+              <button type="button" className="button button_type_edit-profile"></button>
+              <p className="profile__job"></p>
+            </div>
+          </div>
+          <button type="button" className="button button_type_add-card"></button>
+        </section>
+        <section className="elements">
+          <ul className="cards"></ul>
+        </section>
+      </main>
+      <footer className="footer">
+        <p className="footer__text">&COPY; 2020 Mesto Russia</p>
+      </footer>
+      {/* <!-- Попап редактировать профиль --> */}
+      <div className="popup popup_type_edit-profile">
+        <div className="popup__container">
+          <button type="button" className="button button_type_close-popup"></button>
+          <form action="#" name="edit-profile" className="form" noValidate>
+            <h2 className="popup__heading">Редактировать профиль</h2>
+            <label className="form__field">
+              <input type="text"
+                name="profile-name"
+                id="profile-name-input"
+                placeholder="Имя"
+                className="form__input form__input_el_profile-name"
+                required
+                minLength="2"
+                maxLength="40"
+                autoComplete="off" />
+              <span className="form__input-error profile-name-input-error"></span>
+            </label>
+            <label className="form__field">
+              <input type="text"
+                name="profile-job"
+                id="profile-job-input"
+                placeholder="Вид деятельности"
+                className="form__input form__input_el_profile-job"
+                required
+                minLength="2"
+                maxLength="200"
+                autoComplete="off" />
+              <span className="form__input-error profile-job-input-error"></span>
+            </label>
+            <button type="submit" className="button button_type_submit">Сохранить</button>
+          </form>
+        </div>
+      </div>
+      {/* <!-- Попап добавить карточку --> */}
+      <div className="popup popup_type_add-card">
+        <div className="popup__container popup__container_size_middle">
+          <button type="button" className="button button_type_close-popup"></button>
+          <form action="#" name="add-card" className="form" noValidate>
+            <h2 className="popup__heading popup__heading_type_add-card">Новое место</h2>
+            <label className="form__field">
+              <input type="text"
+                name="card-name"
+                id="card-name-input"
+                className="form__input form__input_size_small form__input_el_card-name"
+                placeholder="Название"
+                required
+                minLength="2"
+                maxLength="30"
+                autoComplete="off" />
+              <span className="form__input-error card-name-input-error"></span>
+            </label>
+            <label className="form__field">
+              <input type="url"
+                name="card-link"
+                id="card-link-input"
+                className="form__input form__input_size_small form__input_el_card-link"
+                placeholder="Ссылка на картинку"
+                required
+                autoComplete="off" />
+              <span className="form__input-error card-link-input-error"></span>
+            </label>
+            <button type="submit" className="button button_type_submit">Создать</button>
+          </form>
+        </div>
+      </div>
+      {/* <!-- Попап картинка --> */}
+      <div className="popup popup_type_show-card">
+        <div className="popup__container popup__container_size_large">
+          <button type="button" className="button button_type_close-popup"></button>
+          <figure className="popup__figure">
+            <img src="#" alt="#" className="popup__image" />
+            <figcaption className="popup__caption"></figcaption>
+          </figure>
+        </div>
+      </div>
+      {/* <!-- Попап удаления карточки --> */}
+      <div className="popup popup_type_delete-card">
+        <div className="popup__container popup__container_size_content">
+          <button type="button" className="button button_type_close-popup"></button>
+          <h2 className="popup__heading">Вы уверены?</h2>
+          <button type="button" className="button button_type_submit">Да</button>
+        </div>
+      </div>
+      {/* <!-- Попап обновить аватар --> */}
+      <div className="popup popup_type_update-avatar">
+        <div className="popup__container popup__container_size_content">
+          <button type="button" className="button button_type_close-popup"></button>
+          <form action="#" name="update-avatar" className="form" noValidate>
+            <h2 className="popup__heading">Обновить аватар</h2>
+            <label className="form__field">
+              <input type="url"
+                name="avatar-link"
+                id="avatar-link-input"
+                className="form__input form__input_size_small form__input_el_avatar-link"
+                placeholder="Ссылка на аватар"
+                required
+                autoComplete="off" />
+              <span className="form__input-error avatar-link-input-error"></span>
+            </label>
+            <button type="submit" className="button button_type_submit">Сохранить</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
